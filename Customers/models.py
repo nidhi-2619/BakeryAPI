2eeee1ae1b6363bddab2e008ce8db7cce839f18c
nidhi-2619 <91557785+nidhi-2619@users.noreply.github.com>
2023-07-01 19:00:43 +0530

    customers app is removed for now

--- Customers/models.py
+++ Customers/models.py
@@ -1,19 +1,19 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.customer} - {self.pk}'
-
-
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+# from django.db import models
+# from django.contrib.auth.models import User
+# # Create your models here.
+#
+# class Product(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=8, decimal_places=2)
+# class Order(models.Model):
+#     customer = models.ForeignKey(User, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return f'{self.customer} - {self.pk}'
+#
+#
+# class OrderItem(models.Model):
+#     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField()
--- Customers/urls.py
+++ Customers/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from .views import CustomerRegistrationAPIView,PlaceOrderAPIView, OrderHistoryAPIView
-
-urlpatterns = [
-    path('customers/register/', CustomerRegistrationAPIView.as_view(), name='customer-register'),
-    path('orders/place/', PlaceOrderAPIView.as_view(), name='place-order'),
-    path('orders/history/', OrderHistoryAPIView.as_view(), name='order-history'),
-]
+# from django.urls import path
+# from .views import CustomerRegistrationAPIView,PlaceOrderAPIView, OrderHistoryAPIView
+#
+# urlpatterns = [
+#     path('customers/register/', CustomerRegistrationAPIView.as_view(), name='customer-register'),
+#     path('orders/place/', PlaceOrderAPIView.as_view(), name='place-order'),
+#     path('orders/history/', OrderHistoryAPIView.as_view(), name='order-history'),
+# ]
