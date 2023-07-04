3583c738dfa2ddfb8db73e00bf766c56e0b043aa
nidhi-2619 <91557785+nidhi-2619@users.noreply.github.com>
2023-07-05 00:45:13 +0530

    settings updated

--- BakeryAPI/settings.py
+++ BakeryAPI/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework.authtoken',
-    # 'rest_framework_simplejwt',
+    'rest_framework_simplejwt',
     'Customers',
 ]
 
