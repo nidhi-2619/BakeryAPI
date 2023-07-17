0a6473122b3f15058e546b485250e5a1a0da9f07
nidhi-2619 <91557785+nidhi-2619@users.noreply.github.com>
2023-07-17 08:50:33 +0530

    fix: jwt authentication

--- BakeryAPI/urls.py
+++ BakeryAPI/urls.py
@@ -16,20 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
-# from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from Bakery import urls
+from Customers import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/customers/', include('Customers.urls')),
+    path('customers/', include('Customers.urls')),
     path('', include('Bakery.urls')),
     path('api/', include('Bakery.urls')),
-    # path('api-auth/', include('rest_framework.urls')),
-
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
